0e509f731a3659dc15d106dcaa6aa22f8afef5dd
Melka Tolessa <melkatole1@gmail.com>
2023-03-25 20:01:07 +0300

    more updates

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,11 +1,13 @@
  import React from "react";
- import { Inner, Container, Pane, Title, SubTitle, Image } from "./styles/Jumbotron";
+ import { Inner, Item, Container, Pane, Title, SubTitle, Image } from "./styles/Jumbotron";
 
  export default function Jumbotron({ children, direction = 'row', ...restProps }){
     return (
-        <Inner direction={direction}>
-            {children}
-        </Inner>
+        <Item>
+            <Inner direction={direction}>
+                {children}
+            </Inner>
+        </Item>
     )
  }
 
