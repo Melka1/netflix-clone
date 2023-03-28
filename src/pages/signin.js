16c1fec92ca7c20ead9543e258544474cd96c549
Melka Tolessa <melkatole1@gmail.com>
2023-03-28 15:18:16 +0300

    signin and signup

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import HeaderContainer from "../containers/header";
+import FooterContainer from "../containers/footer";
+import Form from "../components/form";
+import {useNavigate} from 'react-router-dom';
+import * as ROUTES from "../constants/routes";
+import { FirebaseContext } from "../context/firebase";
 
 export default function SignIn(){
+    const navigate = useNavigate();
+    const {firebase} = useContext(FirebaseContext);
+    const [emailAddress, setEmailAddress] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const isInvalid = password===''||emailAddress==='';
+
+    const handleSignIn = (event) =>{
+        event.preventDefault();
+
+        firebase
+            .auth().signInWithEmailAndPassword(emailAddress, password)
+            .then((res) => {
+                console.log(res);
+                navigate(ROUTES.BROWSE)
+            })
+            .catch((err) => {
+                setEmailAddress('');
+                setPassword('');
+                setError(err.message);                    
+            })
+    }
+
     return (
-        <h1>I am Sign In</h1>
+        <>
+            <HeaderContainer>
+                <Form>
+                     <Form.Title>Sign In</Form.Title>
+                     {error && <Form.Error>{error}</Form.Error>}
+                    <Form.Base onSubmit={handleSignIn}>
+                        <Form.Input 
+                            placeholder="Email address"
+                            value={emailAddress}
+                            onChange={({target})=>setEmailAddress(target.value)} 
+                        />
+                        <Form.Input 
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={({target})=>setPassword(target.value)} 
+                        />
+                        <Form.Submit disabled={isInvalid} type="submit">
+                            Sign In
+                        </Form.Submit>
+                    </Form.Base>
+                    <Form.Text>
+                        New to Netflix? <Form.Link to={ROUTES.SIGN_UP}>Sign up now.</Form.Link>
+                    </Form.Text>
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCH to ensure you're not a bot. Learn more.
+                    </Form. TextSmall>
+                </Form>
+            </HeaderContainer>
+            <FooterContainer/>
+        </>
     )
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,82 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import HeaderContainer from "../containers/header";
+import FooterContainer from "../containers/footer";
+import Form from "../components/form";
+import {useNavigate} from 'react-router-dom';
+import * as ROUTES from "../constants/routes";
+import { FirebaseContext } from "../context/firebase";
+
 
 export default function SignUp(){
+    const navigate = useNavigate();
+    const {firebase} = useContext(FirebaseContext);
+
+    const [firstName, setFirstName] = useState("");
+    const [emailAddress, setEmailAddress] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const isInvalid  =  firstName==''||emailAddress==''||password==''
+
+    const handleSignUp = (event) => {
+        event.preventDefault();
+
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(emailAddress, password)
+            .then(result=>{
+                result.user.updateProfile({
+                    displayName: firstName,
+                    photoURL: Math.ceil(Math.random()*5)
+                })
+                .then(()=>{
+                    navigate(ROUTES.BROWSE)
+                })
+            })
+            .catch(error =>{
+                setEmailAddress('');
+                setFirstName('');
+                setPassword('');
+                setError(error.message);
+            })
+    }
+
     return (
-        <h1>I am Sign Up</h1>
+        <>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Sign Up</Form.Title>
+                    {error && <Form.Error>{error}</Form.Error>}
+                    <Form.Base onSubmit={handleSignUp}>
+                        <Form.Input
+                            placeholder="First name"
+                            value={firstName} 
+                            onChange={({target})=>setFirstName(target.value)}
+                        />
+                        <Form.Input 
+                            placeholder="Email address"
+                            value={emailAddress}
+                            onChange={({target})=>setEmailAddress(target.value)} 
+                        />
+                        <Form.Input 
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={({target})=>setPassword(target.value)} 
+                        />
+                        <Form.Submit disabled={isInvalid} type="submit">
+                            Sign Up
+                        </Form.Submit>
+                        <Form.Text>
+                            Already a user? <Form.Link to={ROUTES.SIGN_IN}>Sign in now.</Form.Link>
+                        </Form.Text>
+                        <Form.TextSmall>
+                            This page is protected by Google reCAPTCH to ensure you're not a bot. Learn more.
+                        </Form. TextSmall>
+                    </Form.Base>
+                </Form>
+            </HeaderContainer>
+            <FooterContainer/>
+        </>
     )
 }
