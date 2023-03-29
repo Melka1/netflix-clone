f37dadec54edf015b2c7d8abf620f27549017255
Melka Tolessa <melkatole1@gmail.com>
2023-03-30 00:40:55 +0300

    more updates

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link as ReactRouterLink} from "react-router-dom";
-import {Background, ButtonLink, Frame, Link, Container, Logo} from './styles/header'
+import {Background, Profile, Picture, Dropdown, Group, ButtonLink, Feature, Container, Logo, Text, FeatureCallOut, Link} from './styles/header'
 
 export default function Header({bg = true, children, ...restProps}) {
     return bg ? (<Background {...restProps}>{children}</Background>) : children;
@@ -9,6 +9,12 @@ export default function Header({bg = true, children, ...restProps}) {
 Header.Frame = function HeaderFrame({children, ...restProps}){
     return (
         <Container {...restProps}>{children}</Container>
+    )
+}
+
+Header.Feature = function HeaderFeature({children, ...restProps}) {
+    return (
+        <Feature {...restProps}>{children}</Feature>
     )
 }
 
@@ -23,4 +29,34 @@ Header.ButtonLink = function HeaderButtonLink({to, children, ...restProps}) {
     return (
         <ButtonLink to={to} {...restProps}>{children}</ButtonLink>
     )
+}
+
+Header.Text = function HeaderText({children, ...restProps}){
+    return <Text {...restProps}>{children}</Text>
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({children, ...restProps}){
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
+}
+
+Header.Group = function HeaderGroup({children, ...restProps}){
+    return (
+        <Group {...restProps}>{children}</Group>
+    )
+}
+
+Header.TextLink = function HeaderTextLink({children, ...restProps}){
+    return <Link {...restProps}>{children}</Link>
+}
+
+Header.Picture = function HeaderPicture({...restProps}){
+    return <Picture {...restProps}/>
+}
+
+Header.Dropdown = function HeaderDropdown({children, ...restProps}){
+    return <Dropdown {...restProps}>{children}</Dropdown>
+}
+
+Header.Profile = function HeaderProfile({children, ...restProps}){
+    return <Profile {...restProps}>{children}</Profile>
 }
--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -7,6 +7,10 @@ export const Background = styled.div`
     background:url(${({src})=>{
         return src?`../images/misc/${src}.jpg`:'../images/misc/home-bg.jpg';
     }}) top left / cover no-repeat;
+
+    @media (max-width:1100px){
+        ${({dontShow})=>dontShow && `background:none`}
+    }
 `;
 
 export const Container = styled.div`
@@ -27,6 +31,12 @@ export const Container = styled.div`
 `;
 
 export const Frame = styled.div``;
+
+export const Group = styled.div`
+    display:flex;
+    align-items:center;
+    // margin-right:20px;
+`;
 
 export const Logo = styled.img`
     height: 32px;
@@ -57,3 +67,82 @@ export const ButtonLink = styled(ReactRouterLink)`
         background-color:#f40612;
     }
 `;
+
+export const Feature = styled(Container)`
+    padding:150px 0 500px 0;
+    flex-direction:column;
+    align-items:normal;
+    width:50%;
+
+    @media (max-width:1100px) {
+        display: none;
+    }
+`;
+
+export const Text = styled.p`
+    color:white;
+    font-size:22px;
+    line-height:normal;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+    margin:0;
+`;
+
+export const FeatureCallOut = styled(Text)`
+    font-size:50px;
+    font-weight:bold;
+    margin-bottom:20px;
+`;
+
+export const Link = styled.p`
+    color:white;
+    text-decoration:none;
+    margin-right:30px;
+    font-weight: ${({active})=>(active=='true' ? '700': 'normal')};
+    cursor:pointer;
+
+    &:hover {
+        font-weight:bold;
+    }
+
+`;
+
+export const Picture = styled.button`
+    background:url(${({src})=>src});
+    background-size:contain;
+    background-repeat:no-repeat;
+    background-position:center;
+    border:0;
+    width:32px;
+    height:32px;
+    cursor:pointer;
+    box-sizing:border-box;
+`;
+
+export const Dropdown = styled.div`
+    display:none;
+    background-color:black;
+    position:absolute;
+    padding:10px;
+    width:150px;
+    top:32px;
+    right:10px;
+    box-sizing:border-box;
+`;
+
+export const Profile = styled.div`
+    display:flex;
+    align-items:center;
+    margin-left:20px;
+    position:relative;
+
+    button {
+        cursor:pointer;
+    }
+
+    &:hover > ${Dropdown} {
+        display:flex;
+        flex-direction:column;
+    }
+
+`;
+
